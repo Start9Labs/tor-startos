eccc59a5e2aaa34fd5e1ac5f3d9a079d4e8a86b4
Matt Hill <mattnine@protonmail.com>
2026-05-18 19:35:30 -0600

    chore: bump start-sdk to 1.5.2

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_4_9_8 } from './v0.4.9.8'
+import { v_0_4_9_8_1 } from './v0.4.9.8_1'
 
 export const versionGraph = VersionGraph.of({
-  current: v_0_4_9_8,
+  current: v_0_4_9_8_1,
   other: [],
 })
---
+++ startos/versions/v0.4.9.8_1.ts
@@ -0,0 +1,16 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+
+export const v_0_4_9_8_1 = VersionInfo.of({
+  version: '0.4.9.8:1',
+  releaseNotes: {
+    en_US: 'Bumps start-sdk → 1.5.2.',
+    es_ES: 'Actualiza start-sdk → 1.5.2.',
+    de_DE: 'Aktualisiert start-sdk → 1.5.2.',
+    pl_PL: 'Aktualizuje start-sdk → 1.5.2.',
+    fr_FR: 'Met à jour start-sdk → 1.5.2.',
+  },
+  migrations: {
+    up: async ({ effects }) => {},
+    down: IMPOSSIBLE,
+  },
+})
